22989b7797048ea988f081eba66d7b716431dc27
Jon Peirce <jon.peirce@gmail.com>
2014-08-29 17:22:46 +0100

    'release version 1.78.01'

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -9,7 +9,7 @@
 import os, sys
 
 #version info for PsychoPy
-__version__='1.78.00'
+__version__='1.78.01'
 __license__='GNU GPLv3 (or more recent equivalent)'
 __author__='Jonathan Peirce'
 __author_email__='jon@peirce.org.uk'
@@ -17,7 +17,7 @@ __maintainer_email__='psychopy-dev@googlegroups.com'
 __users_email__='psychopy-users@googlegroups.com'
 __url__='http://www.psychopy.org'
 __downloadUrl__='http://code.google.com/p/psychopy/downloads'
-__git_sha__='87513d0'
+__git_sha__='53b1296'
 __build_platform__='n/a'
 
 __all__ = ["gui", "misc", "visual", "core", "event", "data", "filters", "sound"]
