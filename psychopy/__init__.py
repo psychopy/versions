08ab05964bf7d51721b27a812588b0c40729bfc7
Erik Kastman <erik.kastman@gmail.com>
2016-08-29 11:55:01 -0400

    release 1.83.04

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -9,7 +9,7 @@
 import os, sys
 
 #version info for PsychoPy
-__version__='1.83.03'
+__version__='1.83.04'
 __license__='GNU GPLv3 (or more recent equivalent)'
 __author__='Jonathan Peirce'
 __author_email__='jon@peirce.org.uk'
@@ -17,7 +17,7 @@ __maintainer_email__='psychopy-dev@googlegroups.com'
 __users_email__='psychopy-users@googlegroups.com'
 __url__='http://www.psychopy.org'
 __downloadUrl__='https://github.com/psychopy/psychopy/releases/'
-__git_sha__='e334c9b'
+__git_sha__='6613595'
 __build_platform__='n/a'
 
 __all__ = ["gui", "misc", "visual", "core", "event", "data", "filters", "sound"]
