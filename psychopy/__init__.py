d1d527c7fe972792b4bcc654079f1f9a0a7287b8
Jon Peirce <jon.peirce@gmail.com>
2019-05-13 12:46:26 +0100

    Release 3.1.1

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,14 +12,14 @@
 import os
 import sys
 
-__version__ = '3.1.0'
+__version__ = '3.1.1'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
 __author__ = 'Jonathan Peirce'
 __author_email__ = 'jon.peirce@gmail.com'
 __maintainer_email__ = 'jon.peirce@gmail.com'
 __url__ = 'http://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
-__git_sha__ = 'dcbd8412c'
+__git_sha__ = 'dd0843b28'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
