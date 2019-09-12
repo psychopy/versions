0e3faf33b45c1d39e5cfbf05b92ad9f24b42c612
Jon Peirce <jon.peirce@gmail.com>
2019-09-12 15:46:01 +0100

    Bug-fix release 3.2.3

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,14 +12,14 @@
 import os
 import sys
 
-__version__ = '3.2.2'
+__version__ = '3.2.3'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
 __author__ = 'Jonathan Peirce'
 __author_email__ = 'jon.peirce@gmail.com'
 __maintainer_email__ = 'jon.peirce@gmail.com'
 __url__ = 'http://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
-__git_sha__ = '1cad76342'
+__git_sha__ = '885d630c2'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
