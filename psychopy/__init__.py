e7f7969b3ce5f12d1b8fadf9523d724406a740fd
Jon Peirce <jon.peirce@gmail.com>
2020-11-13 17:27:57 +0000

    Release 2020.2.5

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,14 +12,14 @@
 import os
 import sys
 
-__version__ = '2020.2.4'
+__version__ = '2020.2.5'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
 __author__ = 'Jonathan Peirce'
 __author_email__ = 'jon.peirce@gmail.com'
 __maintainer_email__ = 'jon.peirce@gmail.com'
 __url__ = 'http://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
-__git_sha__ = '669894117'
+__git_sha__ = '2cb74147a'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
