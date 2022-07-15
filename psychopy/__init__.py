3a5f8f1c6002b1136b94085578320c5d1064ebc4
Jon Peirce <jon.peirce@gmail.com>
2022-07-15 13:50:42 +0100

    Release 2022.2.2

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,14 +12,14 @@
 import os
 import sys
 
-__version__ = '2022.2.1'
+__version__ = '2022.2.2'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
 __author__ = 'Open Science Tools Ltd'
 __author_email__ = 'support@opensciencetools.org'
 __maintainer_email__ = 'support@opensciencetools.org'
 __url__ = 'https://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
-__git_sha__ = '336176449'
+__git_sha__ = 'fb6af16ae'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
