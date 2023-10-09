66e4667f91f2ea27f65161f3458a83b4007d018b
Jon Peirce <jon.peirce@gmail.com>
2023-10-09 17:20:13 +0100

    Release 2023.2.2

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,14 +12,14 @@
 import os
 import sys
 
-__version__ = '2023.2.1'
+__version__ = '2023.2.2'
 __license__ = 'GPL v3'
 __author__ = 'Open Science Tools Ltd'
 __author_email__ = 'support@opensciencetools.org'
 __maintainer_email__ = 'support@opensciencetools.org'
 __url__ = 'https://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
-__git_sha__ = '79b5e0521'
+__git_sha__ = 'b2d54f838'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
@@ -42,8 +42,13 @@ if __git_sha__ == 'n/a':
 # update preferences and the user paths
 if 'installing' not in locals():
     from psychopy.preferences import prefs
-    for pathName in prefs.general['paths']:
-        sys.path.append(pathName)
+    for _pathName in prefs.general['paths']:
+        sys.path.append(_pathName)
+    # add paths from plugins/packages (installed by plugins manager)
+    import pathlib as _pathlib
+    for _pathName in _pathlib.Path(prefs.paths['packages']).glob("*"):
+        if _pathName.is_dir():
+            sys.path.append(str(_pathName))
 
     from psychopy.tools.versionchooser import useVersion, ensureMinimal
 
