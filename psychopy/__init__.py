d363c6e9efc5f58f89614e016ca8d815d8844138
Jon Peirce <jon.peirce@gmail.com>
2024-04-30 09:39:36 +0100

    Release 2024.1.4

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,14 +12,14 @@
 import os
 import sys
 
-__version__ = '2024.1.3'
+__version__ = '2024.1.4'
 __license__ = 'GPL v3'
 __author__ = 'Open Science Tools Ltd'
 __author_email__ = 'support@opensciencetools.org'
 __maintainer_email__ = 'support@opensciencetools.org'
 __url__ = 'https://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
-__git_sha__ = 'a2e6ccdec'
+__git_sha__ = '8ccc6ec19'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
